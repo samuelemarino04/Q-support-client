fb9e7e55777166b5732813b29713f2db38af6891
samuelemarino04 <samuele12601@gmail.com>
2023-09-01 09:53:31 +0200

    done

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,14 +2,15 @@ import { Routes, Route } from 'react-router-dom'
 import SignupPage from '../pages/SignupPage/SignupPage'
 import LoginPage from '../pages/LoginPage/LoginPage'
 import NotFoundPage from '../pages/NotFoundPage'
-import NewEventForm from '../components/NewEventForm/NewEventForm'
+import NewEventPage from '../pages/Neweventpage/newEventPage'
 import PrivateRoute from './PrivateRoute'
 import SubscriptionForm from '../components/SubscriptionForm/SubscriptionForm'
 import Homepage from '../pages/Homepage/Homepage'
 import EventPage from '../pages/EventPage/EventPage'
 import SubscriptionsPage from '../pages/SubscriptionsPage/SubscriptionsPage'
 import CreativeProfile from '../pages/CreativeProfile/CreativeProfile'
-import EventDetailsPage from '../pages/EventDetailsPage/EventDetailsPage'
+import EventDetailsPage from '../pages/eventDetailsPage/eventDetailsPage'
+
 
 const AppRoutes = () => {
 
@@ -18,12 +19,12 @@ const AppRoutes = () => {
             <Route path={'/'} element={<Homepage />} />
             <Route path={'/signup'} element={<SignupPage />} />
             <Route path={'/login'} element={<LoginPage />} />
-            <Route path={'/saveEvent'} element={<NewEventForm />} />
             <Route path={'/creative/:user_id'} element={<CreativeProfile />} />
             <Route path={'/creative/:_id/subscriptions'} element={<SubscriptionsPage />} />
             <Route path={'/newsubscription'} element={<SubscriptionForm />} />
             <Route path={'/events'} element={<EventPage />} />
             <Route path={'/getOneEvent/:event_id'} element={<EventDetailsPage />} />
+            <Route path={'/newEvent'} element={<NewEventPage />} />
 
 
             <Route path={'/getSubscriptionsByOwner/:owner_id'} element={<SubscriptionsPage />} />
